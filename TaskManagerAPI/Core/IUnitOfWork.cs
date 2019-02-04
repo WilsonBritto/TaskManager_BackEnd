2936918856bfdb6151718d6008cf555acce1da8c
Wilson Britto <mikebwilson94@gmail.com>
2019-02-05 00:39:00 +0530

    Added UnitOfWork Interface and class

---
+++ TaskManagerAPI/Core/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerAPI.Core.Repositories;
+
+namespace TaskManagerAPI.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ITaskRepository Tasks { get; }
+        int Complete();
+    }
+}
---
+++ TaskManagerAPI/Persisitance/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskManagerAPI.Core;
+using TaskManagerAPI.Core.Repositories;
+using TaskManagerAPI.Persisitance.Repositories;
+
+namespace TaskManagerAPI.Persisitance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TaskManagerDBContext _Context;
+
+        public UnitOfWork(TaskManagerDBContext Context)
+        {
+            _Context = Context;
+            Tasks = new TaskRepository(Context);
+        }
+        public ITaskRepository Tasks { get; private set; }
+
+        public int Complete()
+        {
+            return _Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _Context.Dispose();
+        }
+    }
+}
