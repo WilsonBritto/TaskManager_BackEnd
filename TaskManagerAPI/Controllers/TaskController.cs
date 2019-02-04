1210ccfb528772f3dcafe7bd1f6b08ce9cd82a42
Wilson Britto <mikebwilson94@gmail.com>
2019-02-05 01:01:47 +0530

    Added Taskcontroller and simple Get method

---
+++ TaskManagerAPI/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskManagerAPI.Core;
+using TaskManagerAPI.Core.Domain;
+
+namespace TaskManagerAPI.Controllers
+{
+    public class TaskController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TaskController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetTaskById(int Id)
+        {
+            return Ok(_unitOfWork.Tasks.Get(Id));
+        }
+    }
+}
