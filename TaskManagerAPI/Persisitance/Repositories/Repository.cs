ca58a7a3fee1d05a6d2763499fafcb1ef723a93f
Wilson Britto <mikebwilson94@gmail.com>
2019-02-05 00:14:58 +0530

    Added Repository Interface and class

---
+++ TaskManagerAPI/Persisitance/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using TaskManagerAPI.Core.Repositories;
+
+namespace TaskManagerAPI.Persisitance.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _Context;
+
+        public Repository(DbContext Context)
+        {
+            _Context = Context;
+        }
+        public void Add(TEntity entity)
+        {
+            _Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _Context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int Id)
+        {
+            return _Context.Set<TEntity>().Find(Id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _Context.Set<TEntity>().ToList<TEntity>();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
