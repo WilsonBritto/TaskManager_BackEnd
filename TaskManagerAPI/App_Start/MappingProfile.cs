6910a82c1f1f0cdbc8025cbe39c79065cbfb0ae7
Wilson Britto <mikebwilson94@gmail.com>
2019-02-05 19:10:24 +0530

    Added Mapping profile for Task and task resource and initialized

---
+++ TaskManagerAPI/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskManagerAPI.Core.Domain;
+using TaskManagerAPI.Core.Resources;
+
+namespace TaskManagerAPI.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Task, TaskResource>();
+            CreateMap<TaskResource, Task>();
+        }
+    }
+}
--- TaskManagerAPI/Global.asax.cs
+++ TaskManagerAPI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TaskManagerAPI.App_Start;
 
 namespace TaskManagerAPI
 {
@@ -20,6 +22,7 @@ namespace TaskManagerAPI
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             UnityConfig.RegisterComponents();
+            Mapper.Initialize(m => m.AddProfile<MappingProfile>());
         }
     }
 }
