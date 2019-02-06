1cd09234a7d63010c2750974ee39fe6280f247a2
Wilson Britto <mikebwilson94@gmail.com>
2019-02-07 03:14:00 +0530

    Added 2 tests for TaskController.GetTaskById()

--- TaskManagerAPI.Tests/Controllers/TaskControllerTests.cs
+++ TaskManagerAPI.Tests/Controllers/TaskControllerTests.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using TaskManagerAPI.Core.Domain;
+using TaskManagerAPI.Core;
+using TaskManagerAPI.Controllers;
+using System.Web.Http.Results;
+using TaskManagerAPI.Core.Resources;
+using System.Net;
+using AutoMapper;
+using TaskManagerAPI.App_Start;
 
 namespace TaskManagerAPI.Tests.Controllers
 {
+    [TestFixture]
     class TaskControllerTests
     {
+        private Mock<IUnitOfWork> _unitOfWork;
+        private MapperConfiguration _config;
+        private IMapper _mapper;
+        private TaskController _controller;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _config = new MapperConfiguration(cfg =>
+           {
+               cfg.AddProfile<MappingProfile>();
+           });
+            _mapper = _config.CreateMapper();
+
+            _controller = new TaskController(_unitOfWork.Object, _mapper);
+        }
+
+        [Test]
+        public void GetTaskById_IdNotMatches_ReturnsContent()
+        {
+            _unitOfWork.Setup(s => s.Tasks.Get(1)).Returns((Task)null);
+
+            var result = _controller.GetTaskById(1);
+            var resultObj = result as NegotiatedContentResult<ErrorResource>;
+
+            Assert.That(result, Is.TypeOf<NegotiatedContentResult<ErrorResource>>());
+            Assert.That(resultObj.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
+        [Test]
+        public void GetTaskById_IdMatches_ReturnsOk()
+        {
+            var task = new Task { TaskId = 5 };
+            _unitOfWork.Setup(s => s.Tasks.Get(5)).Returns(task);
+
+            var result = _controller.GetTaskById(5);
+            var resultObj = result as OkNegotiatedContentResult<TaskResource>;
+
+            Assert.That(result, Is.TypeOf<OkNegotiatedContentResult<TaskResource>>());
+            Assert.That(resultObj.Content.TaskId, Is.EqualTo(5));
+        }
     }
 }
