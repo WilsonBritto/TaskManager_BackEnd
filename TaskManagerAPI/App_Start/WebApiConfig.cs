5dc145d9940565a8bf12580c906bdd82c5b735c3
Wilson Britto <mikebwilson94@gmail.com>
2019-02-10 04:28:07 +0530

    Added configuration to enable cors in WebApiConfig

--- TaskManagerAPI/App_Start/WebApiConfig.cs
+++ TaskManagerAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 using TaskManagerAPI.App_Start;
@@ -29,6 +30,9 @@ namespace TaskManagerAPI
             config.Formatters.Add(new CustomJsonFormatter());
             config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            var cors = new EnableCorsAttribute("http://localhost:4200", "Accept,Content-Type", "*");
+            config.EnableCors(cors);
         }
     }
 }
